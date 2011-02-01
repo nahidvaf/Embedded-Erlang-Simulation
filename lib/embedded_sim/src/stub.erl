8a118aeb5fb8d1e2ca4dcbdf658b4c6af3197f31
Reza Javaheri <reza@ooodev.com>
2011-02-01 11:49:03 +0100

    Make stub.erl more flexible passing sim and hard configuration as MFA.

--- lib/embedded_sim/src/stub.erl
+++ lib/embedded_sim/src/stub.erl
@@ -1,16 +1,30 @@
 -module(stub).
 
--export([start_port/3]).
+-export([start_port/1, start_port/3, start_server/3]).
+
+-define(GEN_SIM(Mod,Args),
+        {"sim", {gen_fsm,start_link,[{local, Mod},Mod,Args]}},[]).
+-define(GEN_HARD(Args),
+        {"hard",{erlang,port,Args}}).
+
+start_server(Mod, ServerMod, Args) ->
+    SimArgs = [{parent_pid, self()}],
+    Setup = [{"hard", {ServerMod, start_link, Args}}, ?GEN_SIM(Mod, SimArgs)].
 
 start_port(Mod, PortName, PortSettings) ->
-    case os:getenv("EMBEDDED_ENV") of
-        "sim"  ->
-            Args = [{parent_pid, self()},
-                    {portname, PortName},
-                    {port_settings, PortSettings}],
-            gen_fsm:start_link({local, Mod}, Mod, Args, []);
-        "hard" ->
-            open_port(PortName, PortSettings);
-        false ->
-            open_port(PortName, PortSettings)
+    SimArgs = [{parent_pid, self()}],
+    Setup = [?GEN_HARD([PortName, PortSettings]), ?GEN_SIM(Mod, SimArgs)],
+    start_port(Setup).
+
+start_port(Setup) ->
+    init_port(Setup, os:getenv("EMBEDDED_ENV")).
+
+init_port(Setup, false) ->
+     init_port(Setup, "hard");
+init_port(Setup, Env) ->
+    case proplists:get_value(Env, Setup) of
+        undefined ->
+            {error, no_setup};
+        {M, F, A} ->
+            erlang:apply(M, F, A)
     end.

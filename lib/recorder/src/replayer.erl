755db58305843daef02ff09c9e84b599c2487f82
Reza Javaheri <reza@ooodev.com>
2011-02-22 16:17:20 +0100

    Change replayer module to be compatible with the new recorder.

--- lib/recorder/src/replayer.erl
+++ lib/recorder/src/replayer.erl
@@ -6,14 +6,14 @@ start(LogFile) ->
     {ok, MessageList} = file:consult(LogFile),
     lists:foreach(fun apply_message/1, MessageList).
 
-apply_message({trace, {delay, Delay}, {pid, _Process}, {type, send},
-               {msg, Message}, {to, To}}) ->
-    send_message(Delay, To, Message);
+apply_message({trace, {delay, Delay}, {pid, Process}, {type, 'receive'},
+               {msg, Message}}) ->
+    send_message(Delay, Process, Message);
 apply_message(_) ->
     ok.
 
 send_message(Delay, Process, Message)->
-    timer:sleep(Delay),
+    timer:sleep(Delay div 1000),
     Process ! Message.
 
 

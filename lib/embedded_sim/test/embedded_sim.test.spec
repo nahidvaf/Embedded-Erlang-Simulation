0435d3b0e173a150708e42bd2e65f804ba882c38
Rickard Olsson <rickard@ooodev.com>
2011-02-23 22:32:20 +0100

    Add functionality to define which processes should be recorded to which files

--- lib/embedded_sim/test/embedded_sim.test.spec
+++ lib/embedded_sim/test/embedded_sim.test.spec
@@ -24,4 +24,4 @@
 {cover, "embedded_sim.test.coverspec"}.
 
 %{suites, "test", [all]}.
-{cases, "test", 'serial_stub_SUITE', [all]}.
+{cases, "test", [all], [all]}.
--- lib/recorder/src/rec.erl
+++ lib/recorder/src/rec.erl
@@ -1,16 +1,72 @@
+%% ------------------------------------------------------------------------------
+%% Copyright 2011 Rickard Olsson, Reza Javaheri
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%% http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%% ------------------------------------------------------------------------------
+
+%% ------------------------------------------------------------------------------
+%% @author Rickard Olsson <rickard@ooodev.com>
+%% @author Reza Javaheri <reza@ooodev.com>
+%% @doc Records and replays messages
+%% @end
+%% ------------------------------------------------------------------------------
+
 -module(rec).
 
--export([start/2, add_process/1, stop/0]).
+-export([start/2, add_process/2, stop/0]).
 
-start(FileName, Options) ->
+-export([config_server/1]).
+
+-type error() :: term().
+-type filename() :: string().
+
+-spec (start/2 :: ([{all, filename()}|{pid(), filename()}|{rest, filename()}],
+                   [port]|[]) ->
+              {ok, pid()} | {error, error()}).
+start(Config, Options) ->
+    init(Config),
     HandlerFun = fun(Msg, PrevTimeStamp) ->
                          Now = now(),
-                         record_msg(FileName, Msg, PrevTimeStamp, Now, Options),
+                         record_msg(Msg, PrevTimeStamp, Now, Options),
                          Now
                  end,
     dbg:tracer(process,{HandlerFun, now()}).
 
-add_process(Pid) ->
+init(Args) ->
+    Pid = spawn_link(?MODULE, config_server, Args),
+    true = register(rec_config_server, Pid).
+
+config_server(Config) ->
+    NewConfig = receive
+                    {Pid, get_config} ->
+                        Pid ! {config, Config},
+                        Config;
+                    {add, ConfigElement} ->
+                        [Config|ConfigElement]
+                end,
+    config_server(NewConfig).
+
+get_config() ->
+    rec_config_server ! {self(), get_config},
+    receive
+        {config, Config} ->
+            Config
+    after 1000 ->
+            exit(config_server_not_available)
+    end.
+
+add_process(Pid, FileName) ->
+    rec_config_server ! {add, {FileName, Pid}},
     dbg:p(Pid, [m]).
 
 stop() ->
@@ -19,14 +75,23 @@ stop() ->
 %
 % Internal Functions
 %
-record_msg(FileName, Msg, PrevTimeStamp, Now, Options) ->
+record_msg(Msg, PrevTimeStamp, Now, Options) ->
     TimeStamp = timer:now_diff(Now, PrevTimeStamp),
     case validate_msg(Msg, Options) of
         true ->
-            ok = file:write_file(FileName, format_msg(Msg, TimeStamp), [append]);
+            {Process, MsgStr} = process_msg(Msg, TimeStamp),
+            FileName = get_filename(Process, get_config()),
+            ok = file:write_file(FileName, MsgStr, [append]);
         _Else ->
             ok
     end.
+
+get_filename(_, [{all, FileName}]) ->
+    FileName;
+get_filename(Process, Config) ->
+    Rest = proplists:get_value(rest, Config, "rec.log"),
+    proplists:get_value(Process, Config, Rest).
+
 
 validate_msg(Msg, Options) ->
     ZippedOptions =
@@ -54,28 +119,31 @@ get_reg_name(Pid) when (is_pid(Pid)) ->
 get_reg_name(Process) ->
     Process.
 
-format_msg({trace, P, 'send', M, To}, TimeStamp) ->
-    io_lib:format(
-              "{trace,{delay, ~w},{pid, ~w},{type, ~w}" ++
-              ",{msg, ~w},{to, ~w}}.~n",
-              [TimeStamp,
-               get_reg_name(P),
-               'send',
-               M,
-               get_reg_name(To)]);
-format_msg({trace, P, 'receive', {Port, M}}, TimeStamp) when (is_port(Port)) ->
-    io_lib:format(
-              "{trace,{delay, ~w},{pid, ~w},{type, ~w}" ++
-              ",{msg, ~w}}.~n",
-              [TimeStamp,
-               get_reg_name(P),
-               'receive',
-               {port, M}]);
-format_msg({trace, P, 'receive', M}, TimeStamp) ->
-    io_lib:format(
-              "{trace,{delay, ~w},{pid, ~w},{type, ~w}" ++
-              ",{msg, ~w}}.~n",
-              [TimeStamp,
-               get_reg_name(P),
-               'receive',
-               M]).
+process_msg({trace, P, 'send', M, To}, TimeStamp) ->
+    Process = get_reg_name(P),
+    {Process, io_lib:format(
+          "{trace,{delay, ~w},{pid, ~w},{type, ~w}" ++
+          ",{msg, ~w},{to, ~w}}.~n",
+          [TimeStamp,
+           Process,
+           'send',
+           M,
+           get_reg_name(To)])};
+process_msg({trace, P, 'receive', {Port, M}}, TimeStamp) when (is_port(Port)) ->
+    Process = get_reg_name(P),
+    {Process, io_lib:format(
+          "{trace,{delay, ~w},{pid, ~w},{type, ~w}" ++
+          ",{msg, ~w}}.~n",
+          [TimeStamp,
+           Process,
+           'receive',
+           {port, M}])};
+process_msg({trace, P, 'receive', M}, TimeStamp) ->
+    Process = get_reg_name(P),
+    {Process, io_lib:format(
+          "{trace,{delay, ~w},{pid, ~w},{type, ~w}" ++
+          ",{msg, ~w}}.~n",
+          [TimeStamp,
+           Process,
+           'receive',
+           M])}.
